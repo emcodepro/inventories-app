103b407cc6a23d38464970b076181c1d04c70561
Elmar <oydaatofik@gmail.com>
2023-03-05 23:12:59 +0400

    Create .env file configuration.

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -1,13 +1,16 @@
 import {Sequelize} from 'sequelize-typescript';
 import {Inventory} from "../models/Inventory";
+require('dotenv').config();
 
+// @ts-ignore
 const connection = new Sequelize({
-    host: '127.0.0.1',
-    username: 'emcodep',
-    password: '123456',
-    database: 'jobtask',
-    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
     logging: false,
+    port: process.env.DB_PORT,
     models: [Inventory]
 });
 
