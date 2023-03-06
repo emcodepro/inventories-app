c1a6d598396effce7de1ac89fb95218b9557354f
Elmar <oydaatofik@gmail.com>
2023-03-06 16:08:15 +0400

    Install cors for backend.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import router from "./routes/index";
 import connection from "./db/config";
 import {json, urlencoded} from "body-parser";
+import cors from 'cors';
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 
 app.use(urlencoded({extended: true}));
 
